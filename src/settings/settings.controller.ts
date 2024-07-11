34aabbf51f87e0f29f1a213e34e7f4f4694a3c62
wojciechkubiak <wgkubiak@gmail.com>
2024-07-11 17:50:26 +0200

    Meta data

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,6 +1,23 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { SettingsService } from './settings.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('settings')
 @Controller('settings')
 @UseGuards(AuthGuard('jwt'))
-export class SettingsController {}
+export class SettingsController {
+  constructor(private settingsService: SettingsService) {}
+
+  @Get('meta')
+  @HttpCode(HttpStatus.OK)
+  async getMeta() {
+    return this.settingsService.getMeta();
+  }
+}
--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { RepositoryEnum } from 'src/consts';
+import { Repository } from 'typeorm';
+import { Settings } from './settings.entity';
+import { ActivityLevel, Gender } from './settings.enum';
 
 @Injectable()
-export class SettingsService {}
+export class SettingsService {
+  constructor(
+    @Inject(RepositoryEnum.SETTINGS)
+    private settingsRepository: Repository<Settings>,
+  ) {}
+  private logger = new Logger('SettingsService');
+
+  async getMeta(): Promise<{
+    activityLevel: string[];
+    gender: string[];
+  }> {
+    try {
+      const activityLevel = Object.keys(ActivityLevel);
+      const gender = Object.keys(Gender);
+
+      return {
+        activityLevel: activityLevel.splice(
+          activityLevel.length / 2,
+          activityLevel.length,
+        ),
+        gender: gender.splice(activityLevel.length / 2, activityLevel.length),
+      };
+    } catch {
+      this.logger.error(`Failed to load the meta data`);
+      throw new BadRequestException(`Failed to load the meta data`);
+    }
+  }
+}
